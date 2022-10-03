e127da34902732431c877ebfe000f10b0a6dda02
Hasan Çelik <hsn.2903@outlook.com>
2022-10-03 08:42:44 +0300

    compile sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./assets/sass/main.scss";
+import "./assets/sass/main.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
